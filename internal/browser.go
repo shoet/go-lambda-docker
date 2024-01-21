cb1c425ee6e1bcfc82da9932a50f3b051550e1e6
shoet <32211937+shoet@users.noreply.github.com>
2024-01-21 19:05:27 +0900

    Crawlerに準拠

--- internal/browser.go
+++ internal/browser.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	cp "github.com/otiai10/copy"
 	"github.com/playwright-community/playwright-go"
@@ -17,7 +18,13 @@ type PlaywrightClient struct {
 	browser playwright.Browser
 }
 
-func NewPlaywrightClient() (*PlaywrightClient, func() error, error) {
+type PlaywrightClientConfig struct {
+	BrowserLaunchTimeoutSec int
+}
+
+var _ Crawler = (*PlaywrightClient)(nil)
+
+func NewPlaywrightClient(config *PlaywrightClientConfig) (*PlaywrightClient, func() error, error) {
 	browserBaseDir := "/tmp/playwright/browser"
 	runOption := &playwright.RunOptions{
 		SkipInstallBrowsers: true,
@@ -43,10 +50,15 @@ func NewPlaywrightClient() (*PlaywrightClient, func() error, error) {
 	}
 	browserPath := filepath.Join(matches[0], "chrome-linux", "chrome")
 
+	var browserLaunchTimeoutSec float64
+	if config.BrowserLaunchTimeoutSec != 0 {
+		browserLaunchTimeoutSec = float64(config.BrowserLaunchTimeoutSec) * 1000
+	}
+
 	chromiumOptions := playwright.BrowserTypeLaunchOptions{
 		Headless:        playwright.Bool(true),
 		ExecutablePath:  playwright.String(browserPath),
-		Timeout:         playwright.Float(1200000),
+		Timeout:         playwright.Float(float64(browserLaunchTimeoutSec)),
 		ChromiumSandbox: playwright.Bool(false),
 		Args: []string{
 			"--no-sandbox",
@@ -96,23 +108,64 @@ func (p *PlaywrightClient) FetchPage(url string) (playwright.Page, error) {
 	return page, nil
 }
 
+func (p *PlaywrightClient) FetchContents(url string) (string, string, error) {
+	page, err := p.FetchPage(url)
+	if err != nil {
+		return "", "", fmt.Errorf("could not fetch page: %v", err)
+	}
+
+	titles, err := page.Locator("h1").All()
+	if err != nil {
+		return "", "", fmt.Errorf("could not get h1: %v", err)
+	}
+	titleBuilder := strings.Builder{}
+	for _, t := range titles {
+		text, err := t.TextContent()
+		if err != nil {
+			return "", "", fmt.Errorf("could not get text content: %v", err)
+		}
+		if _, err := titleBuilder.WriteString(text); err != nil {
+			return "", "", fmt.Errorf("could not write string: %v", err)
+		}
+	}
+
+	contents, err := page.Locator("p").All()
+	if err != nil {
+		return "", "", fmt.Errorf("could not get p: %v", err)
+	}
+	contentBuilder := strings.Builder{}
+	for _, c := range contents {
+		text, err := c.TextContent()
+		if err != nil {
+			return "", "", fmt.Errorf("could not get text content: %v", err)
+		}
+		if _, err := contentBuilder.WriteString(text); err != nil {
+			return "", "", fmt.Errorf("could not write string: %v", err)
+		}
+	}
+	return titleBuilder.String(), contentBuilder.String(), nil
+}
+
 func Run() error {
-	b, closer, err := NewPlaywrightClient()
+	config := &PlaywrightClientConfig{
+		BrowserLaunchTimeoutSec: 120,
+	}
+	b, closer, err := NewPlaywrightClient(config)
 	if err != nil {
 		return fmt.Errorf("could not create playwright client: %v", err)
 	}
 
-	url := "https://google.com"
-	page, err := b.FetchPage(url)
+	url := "https://news.yahoo.co.jp/articles/0376263c7ac6dfd8b2bcebcec07802f254af2e55"
+	title, content, err := b.FetchContents(url)
 	if err != nil {
-		return fmt.Errorf("could not run scrape: %v", err)
+		return fmt.Errorf("could not fetch contents: %v", err)
 	}
 
-	txt, err := page.Content()
-	if err != nil {
-		return fmt.Errorf("could not get content: %v", err)
-	}
-	fmt.Println(txt[:10])
+	fmt.Println("### title")
+	fmt.Println(title)
+
+	fmt.Println("### content")
+	fmt.Println(content)
 
 	if err := closer(); err != nil {
 		return fmt.Errorf("could not close playwright client: %v", err)
